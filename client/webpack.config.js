591cab0c31181b2daa8b7ba8f2b52504ebf70681
Christofer Bond <chrisbond66@gmail.com>
2022-11-12 17:59:15 -0800

    g

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { plugin } = require("mongoose");
 
 module.exports = {
   entry: "./src/js/index.js",
